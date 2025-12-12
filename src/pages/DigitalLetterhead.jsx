6238b7d75d3f1e41b5ca8e9f0f60c598449410aa
shila <shila@novainternationaldesigns.com>
2025-12-12 17:01:15 -0600

    revert back the css file name for digitalletterhead and purchaseorderform

--- src/pages/DigitalLetterhead.jsx
+++ src/pages/DigitalLetterhead.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./digitalLetterHead.css";
+import "./DigitalLetterHead.css";
 
 function DigitalLetterHead() {
   const { orderId } = useParams();
