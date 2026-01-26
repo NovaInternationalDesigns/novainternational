e12c93f0630261700be65ab5571aa8cfe518e12b
shila <shila@novainternationaldesigns.com>
2026-01-26 11:41:10 -0600

    changed the Checkout.css name to checkout.css in the Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import "./Checkout.css";
+import "./checkout.css";
 
 const Checkout = () => {
   const location = useLocation();
